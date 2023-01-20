1d77c0f09d5b73af2dcccddd529a0f5bd4411580
ManHoff <achoffman91@gmail.com>
2023-01-20 14:32:43 -0500

    added function to display current weather

--- script.js
+++ script.js
@@ -1,39 +1,81 @@
 function render() {
-    localStorage.removeItem('cities');
+  localStorage.removeItem("cities");
 
+  const todaysDate = moment().format("MMM Do YYYY");
   const apiKey = "321d379de73c76df0aafca3395aff8e7";
   let cityList = JSON.parse(localStorage.getItem("cities")) || [];
   let searchedCity = "";
-  
-      
-      function getApi() {
-          let baseUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchedCity}&appid=${apiKey}`;
-          fetch(baseUrl)
-          .then(function (response) {
-              return response.json();
-            })
-            .then(function (data) {
+  console.log(searchedCity);
+
+  function getApi() {
+    let baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apiKey}&units=imperial`;
+    fetch(baseUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
         console.log(data);
         $("#display").empty();
         let weatherData = data;
-       
-           //TODO start here, was working on how to display data to html
-    });
-}
 
-$("#submitBtn").on("click", function (e) {
+        let container = $("#display");
+
+        let containerEl = $("<div>").appendTo(container);
+
+        let containerDisplay = $("displayel")
+        .appendTo(containerEl);
+
+        let dayOf = $("<h4>")
+          .text(todaysDate)
+          .appendTo(containerEl);
+
+        let expCity = $("<p>")
+          .text("Weather for: " + searchedCity)
+          .appendTo(containerEl);
+
+        let degree = $("<p>")
+          .text(Math.round(weatherData.main.temp) + "˚F")
+          .appendTo(containerEl);
+
+        let humid = $("<p>")
+          .text("Humidity: " + weatherData.main.humidity + "%")
+          .appendTo(containerEl);
+
+        let wind = $("<p>")
+          .text("Wind Speed: " + Math.round(weatherData.wind.speed) + " mph")
+          .appendTo(containerEl);
+
+        let icon = $("<img>")
+          .addClass("rounded mx-auto d-block")
+          .attr(
+            "src",
+            "http://openweathermap.org/img/wn/" +
+              weatherData.weather[0].icon +
+              "@2x.png"
+          )
+          .appendTo(containerEl);
+
+        let iconMain = $("<p>")
+          .text(weatherData.weather[0].main)
+    
+          .appendTo(containerEl);
+
+        $("#displayel").append(container);
+      });
+  }
+
+  $("#submitBtn").on("click", function (e) {
     e.preventDefault();
     searchedCity = $("#input").val().trim();
     if (!cityList.includes(searchedCity)) {
-        (cityList).push(searchedCity);
+      cityList.push(searchedCity);
     }
     if (cityList.length > 5) {
-        cityList.shift();
+      cityList.shift();
     }
-    
+
     localStorage.setItem("cities", JSON.stringify(cityList));
-    
-    
+
     console.log(cityList);
     getApi();
   });
